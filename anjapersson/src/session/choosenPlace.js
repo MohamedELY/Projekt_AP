7f6ee16b4d16b712cfd311153337858d0e61a70a
Kavehlito <kajaliliyan@gmail.com>
2023-02-09 14:09:41 +0100

    Bound search to contentpage with session storage

--- anjapersson/src/session/choosenPlace.js
+++ anjapersson/src/session/choosenPlace.js
@@ -1,6 +1,12 @@
+const CheckChoosenPlace = () => {
+    let place = window.sessionStorage.getItem('place');
+    if (typeof place === 'string') return JSON.parse(place);
+    return false;
+};
+
 const SetChoosenPlace = (placeId) =>
     window.sessionStorage.setItem('place', JSON.stringify(placeId));
 
-const RemoveChoosenPlace= () => window.sessionStorage.removeItem('place');
+const RemoveChoosenPlace = () => window.sessionStorage.removeItem('place');
 
-export {SetChoosenPlace, RemoveChoosenPlace };
+export { CheckChoosenPlace, SetChoosenPlace, RemoveChoosenPlace };
