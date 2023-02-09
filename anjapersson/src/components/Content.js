b3772043ff7a4e1560eda358029744a7de6393e7
4t2g <38842545+4t2g@users.noreply.github.com>
2023-02-09 15:04:33 +0100

    Co-authored-by: Mohamed EL Yahiaoui <MohamedELY@users.noreply.github.com>

--- anjapersson/src/components/Content.js
+++ anjapersson/src/components/Content.js
@@ -1,9 +1,5 @@
-import Reviewpopup from '../components/Reviewpopup';
-import { useState } from 'react';
-
 const Content = ({ data }) => {
     //console.log(data);
-    const [Showreviewpopup, setShowreviewpopup] = useState(false);
 
     return (
         <div className="flex center">
@@ -18,30 +14,6 @@ const Content = ({ data }) => {
                 <h1 className="text-3xl font-bold">{data.name}</h1>
                 <h2 className="text-xl font-bold mt-2">{data.address}</h2>
             </div>
-
-            {Showreviewpopup && <Reviewpopup />}
-
-            <section id="hero" className=" pb-32"></section>
-
-            <div className="relative">
-                <h2 className="text-3xl ml-7 font-bold">Reviews</h2>{' '}
-                {/* lägg till variabel för totala recensioner */}
-                <hr className="my-2 border-blue-300"></hr>
-            </div>
-
-            <button
-                class="bg-blue-500 ml-7 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-full"
-                className="text-white"
-                onClick={() => setShowreviewpopup(true)}
-            >
-                Leave a review
-            </button>
-
-            <button class="bg-blue-500 ml-7  hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-full ">
-                <a href="/login" className="text-white">
-                    Sign in
-                </a>
-            </button>
 
             <section id="hero" class=" pb-10"></section>
         </div>
--- anjapersson/src/components/Recension.js
+++ anjapersson/src/components/Recension.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import Reviewpopup from '../components/Reviewpopup';
+import { CheckLoggedInUser } from '../session/user';
+import { BsFillHandThumbsUpFill } from 'react-icons/bs';
+import { BsFillHandThumbsDownFill } from 'react-icons/bs';
 
 const Recension = ({ data }) => {
+    const [Showreviewpopup, setShowreviewpopup] = useState(false);
+    const [isLoggedin, setisLoggedin] = useState(false);
+    useEffect(() => {
+        setisLoggedin(CheckLoggedInUser());
+    }, []);
+
     return (
-        <div className="flex items-center">
-            <div class="flex">
+        <div>
+            <div>
+                <div className="relative">
+                    <h2 className="text-3xl ml-3 font-bold">Reviews</h2>{' '}
+                    {/* lägg till variabel för totala recensioner */}
+                    <hr className="my-2 border-blue-300"></hr>
+                </div>
+
+                {Showreviewpopup && <Reviewpopup />}
+
+                {isLoggedin ? (
+                    <button
+                        className="bg-blue-500 ml-3 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-full text-white"
+                        onClick={() => setShowreviewpopup(true)}
+                    >
+                        Leave a review
+                    </button>
+                ) : (
+                    <button className="bg-blue-500 ml-3  hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-full ">
+                        <a href="/login" className="text-white">
+                            Sign in
+                        </a>
+                    </button>
+                )}
+            </div>
+
+            <div className="flex items-center">
                 {data.length > 0 && (
                     <ul>
                         {data.map((post) => (
@@ -17,17 +52,32 @@ const Recension = ({ data }) => {
                                     alt="Profile"
                                 /> */}
                                 <div class="ml-4">
-                                    <h2 class="text-xl font-bold">
-                                        {post.title} - {post.rating.toString()}
-                                    </h2>
-                                    {/* post.rating ska bytas ut/länkas till en tumme upp eller 
+                                    <section id="hero" class=" pb-3"></section>
+                                    <div className="flex container w-3/4 mx-20 px-4 border-4 rounded-lg  border-blue-500 bg-white">
+                                        <div class="flex flex-col">
+                                            <h3 class="text-lg font-medium mt-2">
+                                                {post.username}
+                                            </h3>
+                                            <h2 class="text-xl font-bold">
+                                                {post.title}
+                                                {post.rating ? (
+                                                    <BsFillHandThumbsUpFill className="bg-green-500 border-green-500 border-2 rounded" />
+                                                ) : (
+                                                    <BsFillHandThumbsDownFill className="bg-red-500 border-red-500 border-2 rounded" />
+                                                )}
+                                            </h2>
+                                            {/* post.rating ska bytas ut/länkas till en tumme upp eller 
                                         tumme ner beronde på true eller false (True=Upp, False=Ner)*/}
-                                    <h3 class="text-lg font-medium mt-2">
-                                        {post.username}
-                                    </h3>
-                                    <p class="text-gray-600">
-                                        {post.description}
-                                    </p>
+
+                                            <p class="text-gray-600">
+                                                {post.description}
+                                            </p>
+                                            <section
+                                                id="hero"
+                                                class=" pb-5"
+                                            ></section>
+                                        </div>
+                                    </div>
                                 </div>
                             </li>
                         ))}
