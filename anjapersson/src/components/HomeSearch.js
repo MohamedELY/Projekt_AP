34e350e36a752ab57bdcf3395e9ea9ddfa44fc56
Kavehlito <kajaliliyan@gmail.com>
2023-02-15 09:09:48 +0100

    Rating on search works 90%

--- anjapersson/src/components/HomeSearch.js
+++ anjapersson/src/components/HomeSearch.js
@@ -1,30 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import { APIGetLocaionBySearch } from '../api/location';
-
+import { APIGetRating } from '../api/recension';
 import { SetChoosenPlace } from '../session/choosenPlace';
 
 const HomeSearch = () => {
     const [locations, setLocations] = useState({});
+    const [rating, setRating] = useState({});
+
+    let like = 0;
+    let dislike = 0;
 
     const getData = async (searchValue) => {
         if (searchValue === undefined || searchValue == null) {
             searchValue = ' ';
         }
-        
+
         const data = await APIGetLocaionBySearch(searchValue);
-       
-           if (data.length === 0) {
+
+        if (data.length === 0) {
             window.confirm("Sorry, no place found. Search again...")
             window.location.reload();
         }
         console.log(data);
         setLocations(data);
     };
+
+
+    useEffect(() => {
+        const GetRatingData = async () => {
+            let info = await APIGetRating();
+            setRating(info);
+
+        };
+        GetRatingData();
+    }, []);
+
+
+
     function SelectedPlace(id) {
         SetChoosenPlace(id);
         window.open('/content', '_self');
     }
+
     return (
         <section id="hero" className=" pb-72 ">
             <div className="flex flex-row justify-center">
@@ -50,30 +68,37 @@ const HomeSearch = () => {
                             alt=""
                             className="h-4/5 rounded-md opacity-60 "
                         ></img>
-                    </div>                 
-                <div className='flex justify-center'>
-                    <SearchBar findLocations={getData} />
-                </div>                     
-                   
-                <div className='flex justify-center mt-4'>
-                    {locations.length > 0 && (
-                        <div className='grid gap-1 grid-cols-2'>
-                            {locations.map((place) => (
-                                <div value={place.id} key={place.id}>
-                                    <div className=" text-white flex flex-col">
-                                        <button className='border border-black p-1 bg-gradient-to-r from-blue-400 to-neutral-800 hover:from-blue-500 hover:to-neutral-900 rounded-lg' onClick={() => SelectedPlace(place.id)}>    
-                                            <p className='font-bold text-2xl'>{place.place}</p>
+                    </div>
+                    <div className='flex justify-center'>
+                        <SearchBar findLocations={getData} />
+                    </div>
+
+                    <div className='flex justify-center mt-4'>
+                        {locations.length > 0 && (
+                            <div className='grid gap-1 grid-cols-2'>
+                                {locations.map((place) => (
+                                    <div value={place.id} key={place.id}>
+                                        <div className=" text-white flex flex-col">
+                                            <button className='border border-black p-1 bg-gradient-to-r from-blue-400 to-neutral-800 hover:from-blue-500 hover:to-neutral-900 rounded-lg' onClick={() => SelectedPlace(place.id)}>
+                                                <p className='font-bold text-2xl'>{place.place}</p>
                                                 <div className='flex flex-row justify-between'>
                                                     <p className='italic'>{place.location}</p>
                                                     <p>{place.country}</p>
+                                                    {rating.forEach(element => {
+                                                        if (element.id === place.id) {
+                                                            like = element.like;
+                                                            dislike = element.dislike;
+                                                        }
+                                                    })}
+                                                    <p>&#128077;{like} &#128078;{dislike}</p>
                                                 </div>
-                                        </button>
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>    
-                            ))}
-                        </div> 
-                    )}
-                </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </section>
